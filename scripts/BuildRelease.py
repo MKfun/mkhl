f67c14f862ec92f9b455b33a5a9b1258b4efed90
den4ik <denzel8028@hotmail.com>
2025-12-08 14:35:59 +0300

    CI release fix

--- scripts/BuildRelease.py
+++ scripts/BuildRelease.py
@@ -133,6 +133,9 @@ class TargetClient:
         files.append(FileToCopy('gamedir/sprites', 'valve_addon/sprites'))
         files.append(FileToCopy('gamedir/ui', 'valve_addon/ui'))
         files.append(FileToCopy('gamedir/commandmenu_default.txt', 'valve_addon/commandmenu_default.txt'))
+        files.append(FileToCopy('gamedir/models', 'valve_addon/models'))
+        files.append(FileToCopy('gamedir/sound', 'valve_addon/sound'))
+        files.append(FileToCopy('gamedir/events', 'valve_addon/events'))
 
         if get_platform_type() == 'windows':
             files.append(FileToCopy(self.script.paths.out_bin + 'client.pdb',
